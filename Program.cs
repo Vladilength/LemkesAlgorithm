2b26563e293a4b97fe2ca583ad6a966c89645c45
pomidorkaza <karpov-vb-1996@mail.ru>
2024-06-06 10:51:13 +0300

    correct swap method to identify z,w variables

--- Program.cs
+++ Program.cs
@@ -1,21 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more informatin
 using System;
+using System.Collections.Generic;
 using System.Linq;
-
-// Matrix<float> matrix = new Matrix<float>(new float[,] {
-//     {2.0f,62.0f, 0.0f,},
-//     {0.0f,0.0f, 0.0f },
-//     {0.0f,0.0f, 0.0f }
-// });
-
-// Matrix<float> matrix2 = new Matrix<float>(new float[,] {
-//     {12.0f,3.0f, 0.0f,},
-//     {0.0f,3.0f, 0.0f },
-//     {0.0f,0.0f, 1.0f }
-// });
-
-// Matrix<float> c = Matrix<float>.Multiply(matrix, matrix2);
-// c.Print();
 Matrix<float> M = new Matrix<float>(new float[,] {
            /*q*//*z0*//*z1*//*z2*/  //z3 z4
    /*w0*/ {2.0f,      0.0f, 0.0f, 1.0f, -1.0f,1.0f},
@@ -84,12 +70,23 @@ z4.variable_type = "z";
 z4.col_index = 5;
 z4.initial_index = 4;
 z4.is_basic = false;
-EqVariable[] basics = { w0, w1, w2, w3 };
-EqVariable[] nonBasics = { z0, z1, z2, z3 };
+List<EqVariable> basics = new List<EqVariable>();
+basics.Add(w0);
+basics.Add(w1);
+basics.Add(w2);
+basics.Add(w3);
+List<EqVariable> nonBasics = new List<EqVariable>();
+nonBasics.Add(z0);
+nonBasics.Add(z1);
+nonBasics.Add(z2);
+nonBasics.Add(z3);
+nonBasics.Add(z4);
 M.SetBasic(basics);
 M.SetNonBasic(nonBasics);
+// M.LemkesAlgorithm();
 M.Swap(z4, w3);
 M.Print();
+
 // w1 -> z0 ()
 //zo = 
 public class Matrix<T> where T : struct,
@@ -100,21 +97,22 @@ public class Matrix<T> where T : struct,
           IFormattable
 {
     public T[,] data;
-    EqVariable[] basics = { };
-    EqVariable[] nonBasics = { };
+   List<EqVariable> basics = new List<EqVariable>();
+    List<EqVariable> nonBasics = new List<EqVariable>();
     public Matrix(T[,] data)
     {
         this.data = data;
     }
-    public void SetBasic(EqVariable[] basics) {
+    public void SetBasic(List<EqVariable> basics) {
         this.basics = basics;
     }
-    public void SetNonBasic(EqVariable[] nonBasics)
+    public void SetNonBasic(List<EqVariable> nonBasics)
     {
         this.nonBasics = nonBasics;
     }
     public void Swap(EqVariable nonBasic, EqVariable basic)
     {
+
         //z0, w1 zo ->get column index okey w1 get row index
         // matrix[w1[rowIndex]] = [-q/z0.coeff, , - 1/ z.coeff, ];
         var rowIndex = basic.row_index;
@@ -134,29 +132,33 @@ public class Matrix<T> where T : struct,
             }
         }
 //#region swap copy w to z
-        var basicCopy = new EqVariable();
-        basicCopy.is_basic = basic.is_basic;
-        basicCopy.row_index = basic.row_index;
-        basicCopy.col_index = basic.col_index;
+        // var basicCopy = new EqVariable();
+        // basicCopy.is_basic = basic.is_basic;
+        // basicCopy.row_index = basic.row_index;
+        // basicCopy.col_index = basic.col_index;
+        // basicCopy.initial_index = basic.initial_index;
 
-        var nonBasicCopy = new EqVariable();
-        nonBasicCopy.is_basic = nonBasic.is_basic;
-        nonBasicCopy.row_index = nonBasic.row_index;
-        nonBasicCopy.col_index = nonBasic.col_index;
+        // var nonBasicCopy = new EqVariable();
+        // nonBasicCopy.is_basic = nonBasic.is_basic;
+        // nonBasicCopy.row_index = nonBasic.row_index;
+        // nonBasicCopy.col_index = nonBasic.col_index;
+        // nonBasicCopy.initial_index = nonBasic.initial_index;
+        // basic.row_index = nonBasicCopy.row_index;
+        // basic.col_index = nonBasicCopy.col_index;
+        // basic.is_basic = false;
+        // basic.variable_type = "z";
+        // basic.initial_index = nonBasicCopy.initial_index;
 
-        basic.row_index = nonBasicCopy.row_index;
-        basic.col_index = nonBasicCopy.col_index;
-        basic.is_basic = false;
-
-
-        nonBasic.row_index = basicCopy.row_index;
-        nonBasic.col_index = basicCopy.col_index;
-        nonBasic.is_basic = true;
+        // nonBasic.row_index = basicCopy.row_index;
+        // nonBasic.col_index = basicCopy.col_index;
+        // nonBasic.is_basic = true;
+        // nonBasic.variable_type = "w";
+        // nonBasic.initial_index = basicCopy.initial_index; 
         // nonBasics 
-        nonBasics = nonBasics.Where(x => x != nonBasic).ToArray();
-        basics = basics.Where(x => x != basic).ToArray();
-        nonBasics.Append(basic);
-        basics.Append(nonBasic);
+        // nonBasics = nonBasics.Where(x => x.initial_index != nonBasic.initial_index).ToArray();
+        // basics = basics.Where(x => x.initial_index != basic.initial_index).ToArray();
+        // nonBasics.Append(basic);
+        // basics.Append(nonBasic);
 //#endregion 
     for(int i=0; i < data.GetLength(0);i++){
         //
@@ -175,17 +177,104 @@ public class Matrix<T> where T : struct,
                 dynamic b = _zCoeff * data[rowIndex, k];
                 data[i, k] = (T)a + b;
             }
-            // dynamic currList = data[i];
     }
+    //z0, w3
+    // nonBasic.col_index = 2;
+    // nonBasics = new EqVariable[0];
+    // basic.col_index = 1; 
+    int basicIndex = basics.FindIndex(x=>x==basic);
+    int nonBasicIndex = nonBasics.FindIndex(x=>x==nonBasic);
+    Console.Write(basicIndex+"INDEX");
+    Console.Write(nonBasicIndex+"NON_BASIC_INDEX");
+    Console.WriteLine();
+    
+    var nonBasicCopy = new EqVariable();
+    var basicCopy = new EqVariable();
+    nonBasicCopy.variable_type = nonBasic.variable_type;
+    nonBasicCopy.initial_index = nonBasic.initial_index;
+
+    basicCopy.variable_type = basic.variable_type;
+    basicCopy.initial_index = basic.initial_index;
+    
+    basics[basicIndex].variable_type = nonBasicCopy.variable_type;
+
+    basics[basicIndex].initial_index = nonBasicCopy.initial_index;
+
+    nonBasics[nonBasicIndex].variable_type = basicCopy.variable_type;
+    nonBasics[nonBasicIndex].initial_index = basicCopy.initial_index;    
     }
+    public void LemkesAlgorithm(){
+        // todo check trivial solution
+        // swap last z with w with max negative value
+        var rowIndex = 0;
+        dynamic minQ = data[0,0]; // q on first row
+        for(int i = 0;i<data.GetLength(0);i++){
+            dynamic x = data[i,0];
+            if(x<minQ){
+                minQ = x;
+                rowIndex = i;
+            }
+        }
+        EqVariable w =  basics.FirstOrDefault(x=>x.row_index == rowIndex);
+        // EqVariable zn = nonBasic
+        EqVariable zn = null; // initialVariable
+        int maxIndex = 1;// variables col index start after q variable
+        foreach (var item in nonBasics)
+        {
+               if(item.col_index> maxIndex){
+                maxIndex = item.col_index;
+                zn = item;
+               }
+        }
+        
+        var wCopy = new EqVariable();
+        wCopy.initial_index = w.initial_index;
+        wCopy.col_index = w.col_index;
+        wCopy.row_index = w.row_index;
+        wCopy.is_basic = w.is_basic;
+        wCopy.variable_type = w.variable_type;
+        Swap(zn, w);
+        //w type ="w" -> z"index"
+        //find min qi/vij
+        // variable wi leaves dictionary-> zi enters dictionary
+        var enterVariable = new EqVariable();
+        enterVariable.variable_type = w.variable_type == "w" ?"z":"w";
+        enterVariable.initial_index = w.initial_index;
+         
+        // if(wCopy.variable_type=="w"){
+
+        // } else if(wCopy.variable)
+    }
+    
     public void Print()
     {
         var rowCount = data.GetLength(0);
         var colCount = data.GetLength(1);
+        Console.Write(" ");
+        Console.Write(" ");
+        Console.Write(" ");
+        for(int j=0;j< colCount;j++){
+            var nonBasic = nonBasics.FirstOrDefault(x=>x.col_index == j);
+            if(nonBasic!=null){
+            Console.Write(String.Format("{0}\t", nonBasic.variable_type+ nonBasic.initial_index));
+            } else {
+                // q
+            Console.Write(String.Format("{0}\t", "q"));
+            }
+        }
+        Console.WriteLine();
+        
         for (int row = 0; row < rowCount; row++)
         {
+            // Console.WriteLine(basics[]);
+            var basic = basics.FirstOrDefault(x=>x.row_index == row);
+            if(basic == null){
+                Console.Write(row);
+                break;
+            }
+            Console.Write(basic.variable_type+ basic.initial_index+" ");
             for (int col = 0; col < colCount; col++)
-                Console.Write(String.Format("{0}\t", data[row, col]));
+                Console.Write( String.Format("{0}\t", data[row, col]));
             Console.WriteLine();
         }
     }
